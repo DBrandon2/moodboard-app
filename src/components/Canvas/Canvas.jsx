1ae1058e149dc16e2bfab786800c531740ee0729
Brandon <137073865+DBrandon2@users.noreply.github.com>
2026-02-12 18:56:06 +0100

    tailwind config update

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -4,7 +4,7 @@ export default function Canvas() {
   const images = useBoardStore((state) => state.images);
 
   return (
-    <div className="w-full h-screen relative bg-neutral-900 overflow-hidden">
+    <div className="w-full h-screen relative bg-neutral-900 overflow-hidden ">
       {images.map((image) => {
         <img
           src={image.url}
